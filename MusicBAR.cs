4534b525585ce1e6fb1004039282a5df00dd5f0d
off5233 <61343087+off5233@users.noreply.github.com>
2020-06-25 11:02:47 +0700

    Update MusicBAR.cs

--- MusicBAR.cs
+++ MusicBAR.cs
@@ -3,25 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MusicBAR : MonoBehaviour
+public class Musicbar : MonoBehaviour
 {
     public Slider MusicbarBG;
     public Slider MusicbarEFF;
-    public static float MusicsBG;
-    public static float MusicsEFF;
-    // Start is called before the first frame update
+    public static float NumberMusicbarBG;
+    public static float NumberMusicbarEFF;
+
     void Start()
     {
         
-
     }
 
-    // Update is called once per frame
     void Update()
     {
-        MusicsBG = MusicbarBG.value;
-        MusicsEFF = MusicbarEFF.value;
-        MUSICBARIN.MusicsBGIN = MusicsBG;
-        MUSICBARIN.MusicsEFFIN = MusicsEFF;
+        NumberMusicbarBG = MusicbarBG.value;
+        MusicSound.MusicsBG = NumberMusicbarBG;
+        NumberMusicbarEFF = MusicbarEFF.value;
     }
 }
